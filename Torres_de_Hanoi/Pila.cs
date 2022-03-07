8f5e50a5b6a009116eff5d235fa284b251eb4ebf
Pablo <74419051+MrChova20@users.noreply.github.com>
2022-03-07 15:15:48 +0100

    InititalCommit

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -9,14 +9,13 @@ namespace Torres_de_Hanoi
     class Pila
     {
         public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
         public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
+             
+        
+       
+      
         public List<Disco> Elementos { get; set; }
-        */
+       
 
         /* TODO: Implementar métodos */
         public Pila()
@@ -26,7 +25,9 @@ namespace Torres_de_Hanoi
 
         public void push(Disco d)
         {
-
+           
+            Elementos.Add(d);
+           
         }
 
         public Disco pop()
